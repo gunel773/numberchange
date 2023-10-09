423b580eb48976e615682cdceed93fc0e73261c2
gunel773 <gunelmg@code.edu.az>
2023-10-09 10:49:42 +0400

    change number

---
+++ source/repos/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int num1 = 5;
+int num2 = 4;
+if (num1 < num2)
+{
+    Console.WriteLine(num1);
+    Console.WriteLine(num2);
+}
+else
+{
+    Console.WriteLine(num2);
+    Console.WriteLine(num1);
+}
